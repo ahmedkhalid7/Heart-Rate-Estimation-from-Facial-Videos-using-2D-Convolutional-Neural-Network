4c7a36beabf98b18860e3a821da23db189ebc5b4
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2023-11-18 16:17:18 +0200

    updated code

--- Face Detection and Tracking/FaceTracker.py
+++ Face Detection and Tracking/FaceTracker.py
@@ -6,7 +6,6 @@ class FaceTracker():
     def __init__(self, frame, face):
         (x, y, w, h) = face
         self.face = (x, y, w, h)
-        # Arbitrarily picked KCF tracking
         self.tracker = cv2.TrackerKCF_create()
         self.tracker.init(frame, self.face)
 
