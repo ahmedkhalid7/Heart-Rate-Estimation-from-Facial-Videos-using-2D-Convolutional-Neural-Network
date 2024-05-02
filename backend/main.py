239173dca2cbede9d25905fcb1cecd915958f380
Ahmed Khalid <72296578+ahmedkhalid7@users.noreply.github.com>
2024-05-03 02:02:41 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -63,5 +63,5 @@ def predict():
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
 # [END gae_flex_quickstart]
