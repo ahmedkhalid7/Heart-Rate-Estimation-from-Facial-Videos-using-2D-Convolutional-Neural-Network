1df3d0d721fdce8b07445977c562031bd605eb22
ahmedkhalid7 <ahmed.khalid.mohamed@outlook.com>
2024-05-06 16:44:10 +0300

    updated main.py

--- webapp/server side/main.py
+++ webapp/server side/main.py
@@ -3,12 +3,13 @@ import numpy as np
 import time
 import cv2
 import os
-from face_detection import crop_regions
+from ROI_extraction import crop_forehead_and_cheeks
 from model_inference import predict_heart_rate
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,17 +18,17 @@ def predict():
 
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         image_name = f"{timestamp}.jpg"
-        image_path = os.path.join("images", image_name)
+        image_path = os.path.join(os.path.dirname(__file__), "images", image_name)
 
         image_file.save(image_path)
 
         forehead_image_name = f"{timestamp}_forehead.jpg"
-        forehead_image_path = os.path.join("images", forehead_image_name)
+        forehead_image_path = os.path.join(os.path.dirname(__file__), "images", forehead_image_name)
         cheeks_image_name = f"{timestamp}_cheeks.jpg"
-        cheeks_image_path = os.path.join("images", cheeks_image_name)
+        cheeks_image_path = os.path.join(os.path.dirname(__file__), "images", cheeks_image_name)
 
         try:
-            forehead, cheeks = crop_regions(image_path)
+            forehead, cheeks = crop_forehead_and_cheeks(image_path)
             if forehead is not None:
                 cv2.imwrite(forehead_image_path, forehead)
 
@@ -35,7 +36,6 @@ def predict():
                 cv2.imwrite(cheeks_image_path, cheeks)
 
             if np.any(forehead) and np.any(cheeks):
-                print(forehead.shape)
                 predicted_heart_rate = predict_heart_rate(forehead_image_path, cheeks_image_path)
 
                 response = jsonify({'heart_rate': predicted_heart_rate})
@@ -56,9 +56,6 @@ def predict():
     response = jsonify({})
     return response, 204  # no content
 
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
